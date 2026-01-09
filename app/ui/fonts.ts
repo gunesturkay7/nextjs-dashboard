9dc10f32cf714588e202d4fab37aca123b6881c7
Gunes Turky <gunesturkay7@gmail.com>
2026-01-09 12:14:00 +0300

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Lusitana, Playfair_Display } from "next/font/google";
+
+export const playfair = Playfair_Display({ weight: "700", subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: "400", subsets: ["latin"] });
